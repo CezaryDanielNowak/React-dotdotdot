54458eff8538d94cc4f332abb23166d2a1d6192f
Cezary Nowak <CezaryDanielNowak@users.noreply.github.com>
2015-12-11 11:53:59 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+import React, {PropTypes, Component, findDOMNode} from 'react/addons'
+import clamp from 'clamp-js'
+let id = 0
+
+export default class Dotdotdot extends Component {
+  /**
+   * multuline text-overflow: ellipsis
+   */
+  static propTypes = {
+    children: PropTypes.node,
+    clamp: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+    ]).isRequired,
+    ellipsis: PropTypes.string
+  }
+
+  static defaultProps = {
+    ellipsis: '\u2026'
+  }
+
+  constructor(props) {
+    super(props)
+    this.id = ++id
+  }
+
+  componentDidMount() {
+    this.dotdotdot()
+  }
+
+  dotdotdot() {
+    let container = findDOMNode(this.refs.container)
+    if (container.length) {
+      /* eslint-disable */
+      console.warn('Please provide exacly one child to dotdotdot')
+      /* eslint-enable */
+      return
+    }
+
+    clamp(container, {
+      clamp: this.props.clamp,
+      truncationChar: this.props.ellipsis
+    })
+  }
+
+  render() {
+    return (
+      <div ref="container">
+        {this.props.children}
+      </div>
+    )
+  }
+}
