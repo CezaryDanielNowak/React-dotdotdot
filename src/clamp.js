93fe4e1ff20b92a82109878137d9f527bec27c6a
Oguz Gelal <o.gelal77@gmail.com>
2019-09-16 12:00:17 -0400

    prevent target.nodeValue (text node) from parsing as HTML to avoid parsing errors
    # Conflicts:
    #	src/clamp.js

--- src/clamp.js
+++ src/clamp.js
@@ -221,7 +221,8 @@
       //Insert the custom HTML before the truncation character
       if (truncationHTMLContainer) {
         target.nodeValue = target.nodeValue.replace(opt.truncationChar, '');
-        element.innerHTML = target.nodeValue + ' ' + truncationHTMLContainer.innerHTML + opt.truncationChar;
+        element.innerText = target.nodeValue;
+        element.innerHTML = element.innerHTML + opt.truncationChar + ' ' + truncationHTMLContainer.innerHTML;
       }
 
       //Search produced valid chunks
